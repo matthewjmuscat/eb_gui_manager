a43dbe66004d98c810e3bbfc433c5f50dc781852
Matthew Muscat <matthew.muscat@mail.mcgill.ca>
2025-01-23 20:24:57 -0800

    added archiving functionality

--- eb_gui_dose_archive_manager.py
+++ eb_gui_dose_archive_manager.py
@@ -10,7 +10,7 @@ def move_files_with_number(source_dir, target_dir, number):
     
     files_moved = 0
     for filename in os.listdir(source_dir):
-        if str(number) in filename:
+        if number in filename:
             src_path = os.path.join(source_dir, filename)
             dst_path = os.path.join(target_dir, filename)
             shutil.move(src_path, dst_path)
@@ -37,13 +37,16 @@ def main():
     dose_dir = "/home/mjm/egs_brachy/eb_GUI/database/dose"
     
     while True:
-        command = input("Enter a number to move files, or type 'clr' to clear the dose directory: ").strip()
+        command = input("Enter 'arc' followed by a number to archive files, 'clr' to clear the dose directory, or a number to retrieve files: ").strip()
+        args = command.split()
         if command == 'clr':
             clear_directory(dose_dir, archive_dir)
+        elif len(args) == 2 and args[0] == 'arc' and args[1].isdigit():
+            move_files_with_number(dose_dir, archive_dir, args[1])
         elif command.isdigit():
             move_files_with_number(archive_dir, dose_dir, command)
         else:
-            print("Invalid input. Please enter a number or 'clr'.")
+            print("Invalid input. Please enter 'arc <number>', a number, or 'clr'.")
 
 if __name__ == "__main__":
     main()
